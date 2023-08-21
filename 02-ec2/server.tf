c8c19f569aa589530493015cee58db93fb36b23f
Aravind <aravind.mandava@outlook.com>
2023-08-21 10:21:51 +0530

    Terraform ami owner

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,23 +1,7 @@
 #Navigate to terraform resource providers
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "test" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
