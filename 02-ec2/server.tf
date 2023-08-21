0d6b0814c692038d8feb92f102882988545c3bba
Aravind <aravind.mandava@outlook.com>
2023-08-21 10:15:18 +0530

    Terraform ami owner

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,5 +1,21 @@
 #Navigate to terraform resource providers
 
+data "aws_ami" "Centos-8" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["973714476881/Centos-8-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["973714476881"] # Canonical
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
