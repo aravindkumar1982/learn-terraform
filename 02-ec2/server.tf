903050e1b9ef349a6780aff7bec5ce05785696b5
Aravind <aravind.mandava@outlook.com>
2023-08-21 10:06:35 +0530

    Terraform ami owner

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -4,10 +4,10 @@ resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
-}
-  tags          = {
-    Name        = "HelloWorld"
+  tags = {
+    Name = "HelloWorld"
   }
+}
 
 
 
