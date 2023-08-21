cc51fcbbf6bfa3260b69cb96f81dd08d87961efc
Aravind <aravind.mandava@outlook.com>
2023-08-21 09:49:31 +0530

    Terraform ec2 creation

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,7 +1,7 @@
 #Navigate to terraform resource providers
 
 resource "aws_instance" "web" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags          = {
