920bcf693120615bea9968cce54227ca5255c982
Aravind <aravind.mandava@outlook.com>
2023-10-06 11:04:29 +0530

    for-each in loops

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -2,6 +2,7 @@ variable "components" {
   default = ["frontend", "mongodb", "catalogue",] //Here input is a list
 }
 
+/*
 resource "aws_instance" "component" {
 
   count = length(var.components) //count is a key word and works as loop
@@ -9,9 +10,9 @@ resource "aws_instance" "component" {
   ami                     = "ami-03265a0778a880afb"
   instance_type           = "t3.small"
   vpc_security_group_ids  =  [ "sg-0e95806cd6cb8f2af" ]
+*/
 
   tags = {
     Name = element(var.components, count.index) //element is keyword used to verify the indexing inside the list[].
   }
 
-}
