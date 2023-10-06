14ffd43f1a0425fc5e6eef809609d0867fb966c7
Aravind <aravind.mandava@outlook.com>
2023-10-06 10:53:51 +0530

    for-each in loops

--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -7,6 +7,6 @@ variable "components" {
 
 resource "aws_security_group" "allow_tls" {
   for_each  = var.components
-  name      = lookup(var.components, each.value["name"], null )
+  name      = lookup(var.components, each.value["name"], null ) // if there is no value it will just show null
 
 }
