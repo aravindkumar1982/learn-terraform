e3e3a5a47b29aed9afc54826c59359103c5f892e
Aravind <aravind.mandava@outlook.com>
2023-10-06 12:57:17 +0530

    Tags

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -43,8 +43,12 @@ resource "aws_instance" "instance" {
 resource "aws_route53_record" "record" {
   for_each = var.components
   zone_id = var.zone_id
-  name    = lookup("frontend.devopsak.tech")
+  name    = "${lookup(each.value, "name", null)}.devopsak.tech"
   type    = "A"
   ttl     = 30
-  records = [lookup(aws_instance.instance, each.key[""])]
+  records = [lookup(lookup(aws_instance.instance, each.key, null), "private_ip", null)]
+}
+
+output "instances" {
+  value = aws_instance.instance
 }
