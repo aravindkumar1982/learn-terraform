6b01924013c4a517e89b5cac3f02700b58923ebd
Aravind <aravind.mandava@outlook.com>
2023-10-06 09:56:26 +0530

    loops in terraform

---
+++ 06-loops/count/main.tf
@@ -0,0 +1,17 @@
+variable "components" {
+  default = ["frontend", "mongodb"]
+}
+
+resource "aws_instance" "component" {
+
+  count = length(var.components) //count is a key word and works as loop
+
+  ami                     = "ami-03265a0778a880afb"
+  instance_type           = "t3.small"
+  vpc_security_group_ids  =  [ "sg-0e95806cd6cb8f2af" ]
+
+  tags = {
+    Name = element(var.components, count.index) //element is keyword used to verify the indexing inside the list[].
+  }
+
+}
